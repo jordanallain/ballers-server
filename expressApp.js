656a4d25e475a41db9ca81dc7a9ccd26b7b53941
jordanallain <jordan.allain@generalassemb.ly>
2018-08-10 14:10:24 -0400

    Rename express app and vanilla app
    - Rename vanilla server to app.js
    - Rename express server to expressApp.js

--- expressApp.js
+++ expressApp.js
@@ -1,7 +1,11 @@
 const fetch = require('node-fetch')
 const btoa = require('btoa')
 const fs = require('fs')
-const http = require('http')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
 
 // store user name and password for mysportsfeed API key in .env file with
 // USERNAME=<username>
@@ -14,13 +18,13 @@ let password = fs.readFileSync('./.env', {encoding: 'utf8'}).split('\n')[1]
 userName = userName.split('=')[1]
 password = password.split('=')[1]
 
-// set up host and port
-const hostname = 'localhost'
-const port = 4741
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
-// create the server
-const server = http.createServer((req, res) => {
-  console.log(req)
+app.get('/players', (req, res) => {
   fetch(`https://api.mysportsfeeds.com/v1.0/pull/nba/2017-2018-regular/cumulative_player_stats.json`, {
      method: "GET",
      mode: "cors",
@@ -34,28 +38,10 @@ const server = http.createServer((req, res) => {
      referrer: "no-referrer"
    })
      .then(function (response) {
-       res.setHeader("Access-Control-Allow-Origin", "*")
-       res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-       res.statusCode = 200
-       res.end(JSON.stringify(response))
+       return response.json()
      })
-     // .then((response) => {
-     //   res.status(200).send(response)
-     // })
+     .then((response) => res.status(200).send(response))
      .catch(console.error)
 })
 
-// handle client errors
-server.on('clientError', (err, socket) => {
-  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n')
-})
-
-// listen on host and port
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
-
-// close server with message
-server.on('close', () => {
-  console.log('Goodbye')
-})
+app.listen(4741, () => console.log(`Example app listening on port 4741!`))
